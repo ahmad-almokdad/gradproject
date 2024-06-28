db65ab04d2fd57c9db876e2e331bcad8277c03ee
ahmad-almokdad <85493686+ahmad-almokdad@users.noreply.github.com>
2024-06-28 19:40:00 +0300

    edit

--- app/Models/favorites.php
+++ app/Models/favorites.php
@@ -47,7 +47,7 @@ class favorites extends Model
  
     public function provider_favorites(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
-        return $this->belongsTo(Provider::class,"provider_id");
+        return $this->belongsTo(Provider::class,"provider_id")->withCount('orders');
     }
     public function users_favorites(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {

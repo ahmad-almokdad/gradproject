1fa92f713b739cba9e18b5c04a0416d4d8624ebe
ahmadalmokdad36 <ahmadalmokdad36@gmail.com>
2024-08-23 15:34:45 +0300

    Updated

--- app/Services/FCMService.php
+++ app/Services/FCMService.php
@@ -33,16 +33,15 @@ class FCMService
             'Authorization: Bearer ' . $accessToken,
             'Content-Type: application/json',
         ];
+        $ch = \curl_init(); // Note the backslash before curl_init()
+        \curl_setopt($ch, CURLOPT_URL, $url);
+        \curl_setopt($ch, CURLOPT_POST, true);
+        \curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        \curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-
-        $result = curl_exec($ch);
-        curl_close($ch);
+        $result = \curl_exec($ch);
+        \curl_close($ch);
 
         return $result;
     }
